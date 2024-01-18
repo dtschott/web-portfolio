25cea31a91c1789d020cc3cc3b7a8e50ea84df1f
Daniel Schott <dtschott5@gmail.com>
2024-01-17 18:29:03 -0800

    Worked on hero section, touched up navbar

---
+++ src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Button from "../Button/Button";
+import PortraitImage from "/assets/photos/Portrait_Image.png";
+import PortraitImageClose from "/assets/photos/Portrait_Image_Close.png";
+import styles from "./hero_section.module.css";
+
+export default function HeroSection() {
+  return (
+    <div className={styles["hero-container"]}>
+      <div className={styles["hero-header"]}>
+        <h2>Hello, I&apos;m</h2>
+        <h1>Daniel Schott</h1>
+        <div className={styles["description"]}>
+          I&apos;m a freelance web designer/developer who loves a challenge. I
+          have a passion for building beautiful and easy-to-use websites that
+          will help bring your ideas and products to life.
+        </div>
+        <Button text="My Portfolio" className="hero-cta" />
+      </div>
+      <div className={styles["portrait-img-container"]}>
+        <picture>
+          <source
+            media="(min-width:768px)"
+            srcSet={PortraitImage}
+            className={styles["portrait-img"]}
+          />
+          <img src={PortraitImageClose} className={styles["portrait-img"]} />
+        </picture>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,5 @@
 import Button from "../components/Button/Button";
-import InputField from "../components/InputField/InputField";
-import Logo from "../components/Logo/Logo";
-import Socials from "../components/Socials/Socials";
+import HeroSection from "../components/HeroSection/HeroSection";
 import TextContent from "../components/TextContent/TextContent";
 import ContentSection from "../components/ContentSection/ContentSection";
 import PortraitImage from "/assets/photos/Portrait_Image.png";
@@ -10,62 +8,7 @@ import PortraitImageClose from "/assets/photos/Portrait_Image_Close.png";
 export default function HomePage() {
   return (
     <>
-      <ContentSection first>
-        <TextContent hero>
-          <Button
-            text="Let's Talk"
-            styles={{ fontSize: "1.25rem", height: "3rem" }}
-          />
-        </TextContent>
-        <picture>
-          <source
-            media="(min-width:768px)"
-            srcSet={PortraitImage}
-            className="portrait-img"
-          />
-          <img src={PortraitImageClose} className="portrait-img" />
-        </picture>
-      </ContentSection>
-      <div>
-        <h1>Buttons</h1>
-        <Button
-          text="Text Only"
-          color="navy"
-          styles={{ padding: "0px 3rem", height: "3rem" }}
-        />
-        <Button text="Text Icon" icon="house" color="white" />
-        <Button text="Icon After" icon="arrow-right" iconAfter />
-        <Button icon="house" />
-      </div>
-      <div>
-        <h1>InputFields</h1>
-        <InputField placeholder="Your Email" required />
-        <InputField placeholder="My Message" textarea />
-        <InputField
-          placeholder="Your Message"
-          textarea
-          required
-          error="Error message!"
-        />
-        <InputField
-          placeholder="Your Big Name"
-          textarea
-          styles={{ fontSize: "1.2rem", maxWidth: "20rem", maxHeight: "10rem" }}
-        />
-      </div>
-      <div>
-        <h1>Logos</h1>
-        <Logo />
-        <Logo inverted />
-        <Logo column />
-        <Logo column inverted />
-      </div>
-      <div>
-        <h1>Socials</h1>
-        <Socials />
-        <Socials color="white" />
-        <Socials color="navy" />
-      </div>
+      <HeroSection />
     </>
   );
 }
